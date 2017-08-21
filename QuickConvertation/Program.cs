dc88ed2595c9757386da1ea689c762f177b3f984
Андрей Власов <andrey11vlasov@yandex.ru>
2017-08-21 22:37:32 +0300

    Add project files.

---
+++ QuickConvertation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickConvertation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<byte> list = NumberStrToByteList("123");
+            foreach (var item in list)
+                Console.WriteLine(item);
+            Console.ReadLine();
+        }
+
+        static List<byte> NumberStrToByteList(string str)
+        {
+            return Array.ConvertAll(str.ToCharArray(), r => Convert.ToByte(Int16.Parse(r.ToString()))).ToList();
+        }
+    }
+}
